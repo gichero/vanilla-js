39e530ba651ad2f818e715a965835aa5295ea848
Amos Gichero <amosgichero@gmail.com>
2020-06-21 14:59:09 -0400

    counter

--- flip-color/app.js
+++ flip-color/app.js
@@ -7,7 +7,6 @@ const color = document.querySelector('.color')
 btn.addEventListener('click', ()=>{
     
     const randomNumber = getRandomNumber();
-    console.log(randomNumber);
     
     document.body.style.backgroundColor = colors[randomNumber];
     color.textContent = colors[randomNumber];
