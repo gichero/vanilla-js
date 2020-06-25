d03d4a7e6ac15f7ed2ac3ef065de50bd0193da15
Amos Gichero <amosgichero@gmail.com>
2020-06-25 14:14:02 -0400

    buttons and menu dynamically added

--- menu/app.js
+++ menu/app.js
@@ -1,3 +1,7 @@
+//get only the unique categories
+//iterate over categories and return buttons
+//make sure to select buttons when they are available
+
 const menu = [
     {
       id: 1,
@@ -71,13 +75,28 @@ const menu = [
       img: "./images/item-9.jpeg",
       desc: `skateboard fam synth authentic semiotics. Live-edge lyft af, edison bulb yuccie crucifix microdosing.`,
     },
+    {
+        id: 10,
+        title: "wagyu steak",
+        category: "dinner",
+        price: 36.99,
+        img: "./images/item-10.jpeg",
+        desc: `skateboard fam synth authentic semiotics. Live-edge lyft af, edison bulb yuccie crucifix microdosing.`,
+      }
   ];
 
   const sectionCenter = document.querySelector(".section-center");
+  const container = document.querySelector(".btn-container")
 
+  //load menu
   window.addEventListener("DOMContentLoaded", ()=>{
-       displayMenuItems(menu)
+       displayMenuItems(menu);
+       displayMenuBtns()
+       
+       
   });
+
+  
 
   const displayMenuItems = (menuItems) =>{
     let displayMenu = menuItems.map( (item)=>{
@@ -95,4 +114,38 @@ const menu = [
       });
       displayMenu = displayMenu.join("")
       sectionCenter.innerHTML = displayMenu
+  }
+  const displayMenuBtns = () =>{
+
+    const categories = menu.reduce( (values, item)=>{
+        if(!values.includes(item.category)) {
+            values.push(item.category)
+        }
+        return values;
+    },["all"]);
+    const categoryBtns = categories.map( (category)=>{
+        return (`<button class="filter-btn" type="button" data-id=${category}>${category}</button>`) 
+    }).join("");
+    container.innerHTML = categoryBtns
+    const filterBtns = document.querySelectorAll(".filter-btn");
+
+    //filter buttons
+     filterBtns.forEach( (btn)=>{
+         btn.addEventListener('click', (e)=>{
+             console.log(e.currentTarget.dataset.id);
+             
+         const category = e.currentTarget.dataset.id;
+         const menuCategory = menu.filter( (menuItem)=>{
+             if(menuItem.category == category){
+                 return menuItem
+             }
+         })
+         if(category === 'all'){
+             displayMenuItems(menu)
+         }else{
+             displayMenuItems(menuCategory)
+         }
+         })
+    })
+
   }
